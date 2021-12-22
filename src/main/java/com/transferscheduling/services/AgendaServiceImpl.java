00f34386fe11653e9749f132dcd44c40b23b168e
Guilherme1997 <guilhermego1997@gmail.com>
2021-12-22 08:00:48 -0300

    Criação das operacoes C e D

--- src/main/java/com/transferscheduling/services/AgendaServiceImpl.java
+++ src/main/java/com/transferscheduling/services/AgendaServiceImpl.java
@@ -24,19 +24,26 @@ public class AgendaServiceImpl implements AgendaService {
 	
 	private void OperacaoTipoA(Agenda agenda) {
 		if( agenda.getDataTransferencia().equals(agenda.getDataAgendamento())) {
-			
-			var taxa = 3;
-			
-			var porcentagem = (3 * agenda.getValor()) / 100;
-			
-			agenda.setTaxa(taxa + porcentagem );
+			calcularTaxaOperacaoTipoA(agenda);
 		}
+	}
+	
+	private void calcularTaxaOperacaoTipoA(Agenda agenda) {
+		var taxa = 3;
+		
+		var porcentagem = (3 * agenda.getValor()) / 100;
+		
+		agenda.setTaxa(taxa + porcentagem );
 	}
 	
 	private void OperacaoTipoB(Agenda agenda) {
 		if(obterRangeOperacaoB(agenda)) {
-			agenda.setTaxa((float) 12);
+			calcularTaxaOperacaoTipoB(agenda);
 		}
+	}
+	
+	private void calcularTaxaOperacaoTipoB(Agenda agenda) {
+		agenda.setTaxa((float) 12);
 	}
 	
 	private boolean obterRangeOperacaoB(Agenda agenda) {
@@ -54,12 +61,51 @@ public class AgendaServiceImpl implements AgendaService {
 				.equals(diaPosterior.plusDays(9))));
 	}
 	
+	private void operacaoTipoC(Agenda agenda) {
+
+		if(agenda.getDataTransferencia().isAfter(LocalDate.now().plusDays(10))) {
+			agenda.setTaxa( (float) (8.2 * agenda.getValor()) / 100);
+		}
+		
+		if(agenda.getDataTransferencia().isAfter(LocalDate.now().plusDays(20))) {
+			agenda.setTaxa( (float) (6.9 * agenda.getValor()) / 100);
+		}
+		
+		if(agenda.getDataTransferencia().isAfter(LocalDate.now().plusDays(30))) {
+			agenda.setTaxa( (float) (4.7 * agenda.getValor()) / 100);
+		}
+		if(agenda.getDataTransferencia().isAfter(LocalDate.now().plusDays(40))) {
+			agenda.setTaxa( (float) (1.7 * agenda.getValor()) / 100);
+		}
+	}
+	
+	private void OperacaoTipoD(Agenda agenda) {
+		if(agenda.getValor() > 0 && agenda.getValor() < 1001) {
+			calcularTaxaOperacaoTipoA(agenda);
+		}
+		
+		if(agenda.getValor() >= 1001 && agenda.getValor() <= 2000) {
+			calcularTaxaOperacaoTipoB(agenda);
+		}
+		
+		if(agenda.getValor() > 2000) {
+			operacaoTipoC(agenda);
+		}
+	}
+	
 	@Override
 	public Agenda agendar(Agenda agenda) {
-
-		OperacaoTipoA(agenda);
 		
-		OperacaoTipoB(agenda);
+		OperacaoTipoD(agenda);
+		
+		if(agenda.getTaxa() == null) {
+			
+			OperacaoTipoA(agenda);
+			
+			OperacaoTipoB(agenda);
+			
+			operacaoTipoC(agenda);
+		}
 		
 		return agendaRepository.save(agenda);
 	
