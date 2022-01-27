d42df6eaa72b47d173deca71076a546744f81926
Guilherme1997 <guilhermego1997@gmail.com>
2022-01-27 20:06:13 -0300

    Inclusão de tratamento de excessoes

--- src/main/java/com/transferscheduling/util/CustomGlobalExceptionHandler.java
+++ src/main/java/com/transferscheduling/util/CustomGlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.transferscheduling.util;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpHeaders;
@@ -14,7 +13,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(Exception.class)
 
